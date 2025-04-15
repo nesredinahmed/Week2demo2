f268b98b6e6d43ef68383b474e319a89819f3100
Nesredin Ahmed <ahmednesredin@icloud.com>
2025-04-14 22:00:59 -0700

    update

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+//NOUN: Person, Place, Thing, Idea
+public class Person{
+    //Adjectives become properties - facts things we know measures
+    String fullName;
+    int age;
 
-public class Person {
+    static double taxRate = 0.08;
+
+    public Person(String fullName, int age){
+        //Adjectives become properties
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    // VERB - Action word become our methods
+    public static double getTaxRate(){
+        return taxRate; //uses "shared" static property.
+    }
+
+    // VERB - Action
+    public void greet(){
+        System.out.printf(
+                "Hello my name is %s, and I am %d years old. My rate is %.2f\n",
+                this.fullName,
+                this.age,
+                getTaxRate());
+    }
 }
